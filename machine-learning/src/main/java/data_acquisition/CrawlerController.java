ab6629f5ac224b0ca92918b0459e51ba6c7f85ad
Robson Mamede <robson.lima@dataprev.gov.br>
2022-08-18 22:10:15 -0300

    Ajustando as classes do crawler

--- machine-learning/src/main/java/data_acquisition/CrawlerController.java
+++ machine-learning/src/main/java/data_acquisition/CrawlerController.java
@@ -9,16 +9,22 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 public class CrawlerController {
 	
 	public static void main(String[] args) throws Exception {
-		 int numberOfCrawlers = 2;
-	    CrawlConfig config = new CrawlConfig();
-	    String crawlStorageFolder = "data";
+		 int numberOfCrawlers = 2; //This controls the number of threads used for the crawl
+	    CrawlConfig config = new CrawlConfig(); 
+	    String crawlStorageFolder = "data"; //The location where crawl data is stored
 	    
 	    config.setCrawlStorageFolder(crawlStorageFolder);
-	    config.setPolitenessDelay(500);
-	    config.setMaxDepthOfCrawling(2);
-	    config.setMaxPagesToFetch(20);
-	    config.setIncludeBinaryContentInCrawling(false);
-
+	    config.setPolitenessDelay(500); //How many seconds to pause between requests
+	    config.setMaxDepthOfCrawling(2); //How deep the crawl will go
+	    config.setMaxPagesToFetch(20); //How many pages to fetch
+	    config.setIncludeBinaryContentInCrawling(false); //How many pages to fetch
+	    
+	    /*
+	     * RobotstxtConfig and RobotstxtServer classes used to handle robot.txt files.
+		  * These files contain instructions that are intended to be read by a web crawler. 
+		  * They provide direction to help a crawler to do a better job such as specifying which parts
+		  * of a site should not be crawled
+	     */
 	    PageFetcher pageFetcher = new PageFetcher(config);
 	    RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
 	    RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
--- machine-learning/src/main/java/data_acquisition/SimpleWebCrawler.java
+++ machine-learning/src/main/java/data_acquisition/SimpleWebCrawler.java
@@ -11,14 +11,14 @@ public class SimpleWebCrawler {
 	private final String topic;
    private final String startingURL;
    private final String urlLimiter;
-   private final int pageLimit = 20;
-   private final ArrayList<String> visitedList = new ArrayList<>();
-   private final ArrayList<String> pageList = new ArrayList<>();
+   private final int pageLimit = 20; //The maximum number of pages to retrieve
+   private final ArrayList<String> visitedList = new ArrayList<>(); //The ArrayList containing pages that have already been visited
+   private final ArrayList<String> pageList = new ArrayList<>(); //An ArrayList containing the URLs of the pages of interest
 
    public SimpleWebCrawler() {
-       startingURL = "https://en.wikipedia.org/wiki/Bishop_Rock,_Isles_of_Scilly";
-       urlLimiter = "Bishop_Rock";
-       topic = "shipping route";
+       startingURL = "https://en.wikipedia.org/wiki/Bishop_Rock,_Isles_of_Scilly"; // The URL of the first page
+       urlLimiter = "Bishop_Rock"; //A string that must be contained in a link before it will be followed
+       topic = "shipping route"; //The keyword that needs to be in a page for the page to be accepted
        visitPage(startingURL);
    }
 
@@ -27,13 +27,14 @@ public class SimpleWebCrawler {
            return;
        }
        if (visitedList.contains(url)) {
-           // URL already visited
+      	 System.out.println("[ALREADY VISITED] " + url);
        } else {
            visitedList.add(url);
            try {
                Document doc = Jsoup.connect(url).get();
                if (doc.text().contains(topic)) {
                    System.out.println((pageList.size() + 1) + ": [" + url + "]");
+                   //System.out.println("\t[TEXT] " + doc.text()); 
                    pageList.add(url);
 
                    // Process page links
